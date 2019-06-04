b6ad8e8e176d3f9eaaee16c2fc1ef040d0648b87
bbakersc <bbaker@sharecare.com>
2019-06-04 12:24:49 -0400

    v1.0.3: Stripping DB stuff out - testing deployment to Dev

--- src/app/factories/mongo-connection-service.js
+++
@@ -1,7 +0,0 @@
-const debug = require('debug')('sc.DroneApiTestA.MongoDBConnectionService');
-
-debug('Build MongoDBConnectionService');
-
-const MongoDBConnectionService = require('@sharecare/mongo');
-
-module.exports = MongoDBConnectionService();
